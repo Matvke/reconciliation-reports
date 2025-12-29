dbbe362d2d29aab9c2a72c7e79c3fef546278d65
Matvey Butolin <matvei31055@gmail.com>
2025-12-29 17:11:53 +0500

    feat(acts): db models done - Create migration - Create db models

--- reconciliation/acts/apps.py
+++ reconciliation/acts/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ActsConfig(AppConfig):
-    name = 'acts'
+    name = "acts"
+    verbose_name = "Акты сверки"
--- reconciliation/acts/models.py
+++ reconciliation/acts/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=64, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "магазин"
+        verbose_name_plural = "Магазины"
+
+    def __str__(self):
+        return self.name
+
+
+class Supply(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=64,
+        verbose_name="ID поставки",
+        help_text="Введите уникальный идентификатор поставки",
+    )
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Сумма")
+    date = models.DateField(verbose_name="Дата")
+    store = models.ForeignKey(
+        Store, verbose_name="Магазин получатель поставки", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "поставка"
+        verbose_name_plural = "Поставки"
+
+    def __str__(self):
+        return f"Поставка номер {self.id} от {self.date}"
+
+
+class Transaction(models.Model):
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Сумма")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата транзакции")
+    store = models.ForeignKey(
+        Store, verbose_name="Магазин плательщик", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f"Платеж от {self.date}. Плательщик {self.store}"

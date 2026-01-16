1cc48f513bf54c498994f0235558e882563ec9a4
Matvey Butolin <matvei31055@gmail.com>
2026-01-16 20:55:11 +0500

    fix: fix typo in settings

--- reconciliation/reconciliation/settings.py
+++ reconciliation/reconciliation/settings.py
@@ -96,7 +96,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "backend_static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
